4e7e9ac54d6867f943b2d604a82ceb5d219ac7ee
ColinHask <colinhaskins2022@gmail.com>
2024-10-09 22:41:33 -0400

    testing changes

--- Initial-arduino-test/Initial-arduino-test.ino
+++ Initial-arduino-test/Initial-arduino-test.ino
@@ -16,13 +16,14 @@ void setup() {
 void loop() {
   //loop to run for an amount of time before changing direction
   // 3350 seems to be a full rotation? (NOT EXACT)
-  for (int i = 0; i <= 3350; i++) {
+  for (int i = 0; i <= 200; i++) {
     // put your main code here, to run repeatedly:
     // must set pin to low and back to high in order to do 1 step
     digitalWrite(3, LOW);
+    delayMicroseconds(1000);
     digitalWrite(3, HIGH);
     // change delay to change motor speed
-    delayMicroseconds(600);
+    delayMicroseconds(1000);
   }
   // swap direction to opposite direction
   if (clockwise == true) {
