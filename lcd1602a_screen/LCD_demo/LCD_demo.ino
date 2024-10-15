0da185828d77be2df35d5b026b4f6fcf790aed55
ColinHask <colinhaskins2022@gmail.com>
2024-10-14 22:18:48 -0400

    update for larger screen / added bobbin circumference value

--- lcd1602a_screen/LCD_demo/LCD_demo.ino
+++ lcd1602a_screen/LCD_demo/LCD_demo.ino
@@ -1,11 +1,14 @@
 #include <LCD_I2C.h>
 
-LCD_I2C lcd(0x27, 16, 2); // default address (for most modules)
+LCD_I2C lcd(0x27, 20, 4); // default address (for most modules)
 
 // bobbin width
 // (total)spins
 // number of wires (in parallel)
 // wire gauge
+// circumference (bobbin)
+
+
 
 void demo(){
   for(int i = 0; i<=100 ;i++){
@@ -13,21 +16,28 @@ void demo(){
     lcd.setCursor(5,0);
     lcd.print(i);
 
+    //bobbin cir value
+    lcd.setCursor(17,0);
+    lcd.print(i-1);
+
     if(i<60){
       //wires value
       lcd.setCursor(6,1);
       lcd.print(i/10);
     }
     //width value
-    lcd.setCursor(14,0);
+    lcd.setCursor(6,2);
     lcd.print(i-1);
 
     if(i<26){
       //gauge value
-      lcd.setCursor(14,1);
+      lcd.setCursor(6,3);
       lcd.print(i-1);
       
     }
+
+
+
     delay(100);
 
   }
@@ -38,16 +48,22 @@ void demo(){
     //spin
     lcd.setCursor(5,0);
     lcd.print("   ");
+    //bobbin cir
+    lcd.setCursor(17,0);
+    lcd.print("   ");
     //wires
     lcd.setCursor(6,1);
     lcd.print("  ");
     //width
-    lcd.setCursor(14,0);
+    lcd.setCursor(6,2);
     lcd.print("  ");
     //gauge
-    lcd.setCursor(14,1);
+    lcd.setCursor(6,3);
     lcd.print("  ");
+
+
     delay(200);
+
 }
 
 void setup()
@@ -55,17 +71,26 @@ void setup()
   lcd.begin();
   lcd.backlight();
   lcd.print("Spin:");
+
+  lcd.setCursor(9,0);
+  lcd.print("Bob Cir:");
+  
   lcd.setCursor(0,1);
   lcd.print("Wires:");
-  lcd.setCursor(8,0);
+
+  lcd.setCursor(0,2);
   lcd.print("Width:");
-  lcd.setCursor(8,1);
+
+  lcd.setCursor(0,3);
   lcd.print("Gauge:");
+
+  lcd.setCursor(13,3);
+  lcd.print("[START]");
 
 }
 
 void loop()
 {
   demo();
-
+  lcd.setCursor(0,3);
 }
